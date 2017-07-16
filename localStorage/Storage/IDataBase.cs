293f100a892b9ce0cadaa50f0f384b35e741b32c
Велор Халилов <halilov.v@tula.itstep.local>
2017-07-16 12:18:26 +0300

    Add some interface and resources

---
+++ localStorage/Storage/IDataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using localStorage.Model;
+
+namespace localStorage.Storage
+{
+    interface IDataBase
+    {
+        //Метод синглтона для получения
+        //Экземпляра класса
+        IDataBase Current();
+
+        bool Login(string username,
+                   string password);
+        User GetCurrentUser();
+        IEnumerable<User> GetAllUsers();
+
+        IEnumerable<LocalRecord> GetLocalRecordsByUserId(Guid userid);
+        IEnumerable<LocalCategory> GetFirstLevelCategoryesByLRecordId(Guid lRecordId);
+        IEnumerable<LocalCategory> GetOtherLevelCategoryesByCategoryId(Guid lCategoryId);
+        IEnumerable<LocalData> GetDataByLCategoryId(Guid categoryId);
+        IEnumerable<LocalFile> GetFilesByDataId(Guid lDataId);
+    }
+}
