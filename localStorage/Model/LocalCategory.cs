f7a992a8233d8ebe8e1a6faaf046eb878b096271
gusev71 <SERGE-GUSEV-007@yandex.ru>
2017-07-20 21:27:45 +0300

    111

--- localStorage/Model/LocalCategory.cs
+++ localStorage/Model/LocalCategory.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace localStorage.Model
 {
-    public class LocalCategory
+    public class Category
     {
         public Guid id { get; set; }
-        public Guid LRecordId { get; set; }
-        public Guid ParentId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        
+        public bool Public { get; set; }
+        public dynamic data { get; set; }
+        public Guid Parent { get; set; }
+        public Guid[] users { get; set; }
+        public Guid creator { get; set; }//owner user
     }
 }
